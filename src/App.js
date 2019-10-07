078e42cc90b2dfd859d32e7bcf8f72ecfbe739a1
Armougom <y.armougom@gmail.com>
2019-10-07 22:31:01 +0200

    exercice création composant Contact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Contact 
+          name= "Charlotte Ramos"
+          image="https://randomuser.me/api/portraits/women/10.jpg"
+          online={false}
+        />
+        <Contact 
+          name= "Kristen Walker"
+          image="https://randomuser.me/api/portraits/women/88.jpg"
+          online={true}
+        />
+        <Contact 
+          name= "Pauline Mitchelle"
+          image="https://randomuser.me/api/portraits/women/17.jpg"
+          online={true}
+        />
+
     </div>
   );
 }
